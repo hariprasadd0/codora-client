1aa98f9e6a01b02aacec04e9a0facf7cfedce5f9
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-03-13 22:29:20 +0530

    feat : add publicRoutes

---
+++ src/routes/publicRoutes.tsx
@@ -0,0 +1,22 @@
+import { lazy } from "react";
+
+const Login = lazy(()=> import("../modules/auth/login"))
+const SignUp = lazy(()=> import("../modules/auth/signUp"))
+const ForgotPassword = lazy(()=> import("../modules/auth/forgotPassword"))
+
+const publicRoutes = [
+    {
+        path: "/login",
+        element: <Login />
+    },
+    {
+        path: "/signup",
+        element: <SignUp />
+    },
+    {
+        path: "/forgot-password",
+        element: <ForgotPassword />
+    }
+]
+
+export default publicRoutes
