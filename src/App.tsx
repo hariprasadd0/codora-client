2a15d95865a6d3274fad1c34ad3958fc0758188a
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-03-21 19:40:47 +0530

    refactor: simplify App component by removing direct auth page imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import { Login } from "./modules/auth/login"
-import { SignUp } from "./modules/auth/signin"
-import { ThemeProvider } from "./components/theme-provider"
+import { Outlet } from "react-router-dom"
 function App() {
 
   return (
-    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      
-      <SignUp />
-      <Login />
-    </ThemeProvider>
-    )
+      <Outlet />
+  )
 }
 
 export default App
