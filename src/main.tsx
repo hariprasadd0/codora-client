3dcd8483b698e65b154f52f786f2ffc32d85e464
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-03-21 19:41:14 +0530

    feat: implement routing with ThemeProvider in main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import appRoutes from './routes/appRoutes.tsx'
+import { ThemeProvider } from './components/theme-provider'
+
+const router = createBrowserRouter(appRoutes)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>,
 )
