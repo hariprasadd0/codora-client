4e332ae389e985a2591acf6c69d63e87cc2e46c9
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-03-21 19:41:37 +0530

    feat: redesign ModeToggle component with card layout for theme selection

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,37 +1,48 @@
-import { Moon, Sun } from "lucide-react"
-
-import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
 import { useTheme } from "@/components/theme-provider"
+import { Card } from "./ui/card"
 
 export function ModeToggle() {
   const { setTheme } = useTheme()
+  const theme = localStorage.getItem("vite-ui-theme")
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <div className="flex items-center gap-4 flex-wrap mt-2 px-2">
+      <div className="flex flex-col gap-3">
+      <Card
+        onClick={() => setTheme("light")}
+        className={`${theme === "light" ? "ring-2 ring-gray-400 ring-offset-2" : ""} flex flex-col bg-[#fafafabd]  shadow-none rounded-md w-[150px] h-[100px] justify-end items-end `}
+      >
+        <div className="bg-white border border-b-0 border-r-0 border-gray-300  select-none w-28 h-20 rounded-tl-sm rounded-br-sm text-black p-3">
+          Aa
+        </div>
+      </Card>
+      <span className="text-sm text-muted-foreground">Light</span>
+      </div>
+      <div className="flex flex-col gap-3">
+     <Card
+        onClick={() => setTheme("dark")}
+        className={`${theme === "dark" ? "ring-1 ring-gray-300 ring-offset-2" : ""} flex flex-col bg-black/50 shadow-none rounded-md w-[150px] h-[100px] justify-end items-end`}
+      >
+        <div className="bg-black border border-b-0 border-r-0 border-gray-300  select-none w-28 h-20 rounded-tl-sm rounded-br-sm text-white p-3">
+          Aa
+        </div>
+      </Card>
+      <span className="text-sm text-muted-foreground">Dark</span>
+      </div>
+      <div className="flex flex-col gap-3">
+      <Card
+        onClick={() => setTheme("system")}
+        className={`${theme === "system" ? "ring-1 ring-gray-300 ring-offset-2" : ""} flex  bg-black/50 shadow-none rounded-md w-[150px] h-[100px] justify-end items-end`}
+      >
+        <div className="bg-black border border-b-0 border-r-0 border-gray-300  select-none w-20 h-20 rounded-tl-sm rounded-br-sm text-white p-3">
+          Aa
+        </div>
+         <div className="bg-white border border-b-0 border-r-0 border-gray-300  select-none w-20 h-20 rounded-tl-sm rounded-br-sm text-black p-3">
+          Aa
+        </div>
+      </Card>
+      <span className="text-sm text-muted-foreground">System</span>
+      </div>
+    </div>
   )
 }
