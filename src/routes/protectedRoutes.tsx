17ab4e34864abee7bc14649b19579312fc11e90d
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-03-24 23:43:38 +0530

    feat: add Tasks route and improve project routing structure

--- src/routes/protectedRoutes.tsx
+++ src/routes/protectedRoutes.tsx
@@ -1,35 +1,37 @@
 import NotFound from '@/components/NotFound';
+import Tasks from '@/modules/tasks/Tasks.tsx';
 import Settings from '@/modules/users/Settings';
-import {lazy} from 'react';
+import { lazy } from 'react';
 
-const Dashboard = lazy(()=>import("../modules/users/Dashboard"))
-const Project = lazy(()=>import("../pages/Project"))
+const Dashboard = lazy(() => import('../modules/users/Dashboard'));
+const Projects = lazy(() => import('../modules/projects/AllProjects.tsx'));
+const Project = lazy(() => import('../modules/projects/Project'));
 
 const protectedRoutes = [
-    {
-        path: "/dashboard",
-        element: <Dashboard />
-    },
-    {
-        path: "/projects",
-        element: <Project/>,
-        children : [
-            {
-                path: ":projectId",
-                element: <Project />
-            }
-        ]
-        
-    },
-    {
-        path: "/settings",
-        element: <Settings />
-    }
-    ,
-    {
-        path: "*",
-        element: <NotFound />
-    }
-]
+  {
+    path: '/dashboard',
+    element: <Dashboard />,
+  },
+  {
+    path: '/projects',
+    element: <Projects />,
+  },
+  {
+    path: '/projects/:id',
+    element: <Project />,
+  },
+  {
+    path: '/settings',
+    element: <Settings />,
+  },
+  {
+    path: '/tasks',
+    element: <Tasks />,
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+];
 
-export default protectedRoutes
+export default protectedRoutes;
