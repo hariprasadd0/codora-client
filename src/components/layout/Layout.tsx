ad7b4e9c3be91158e5b03501da9b783e85216fb2
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-03-25 22:42:14 +0530

    chore: refactored ui

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,23 +1,19 @@
-import { Outlet } from "react-router-dom"
-import {AppSidebar} from "../sidebar/app-sidebar"
-import { SidebarProvider, SidebarTrigger } from "../ui/sidebar"
-
+import { Outlet } from 'react-router-dom';
+import { AppSidebar } from '../sidebar/app-sidebar';
+import { SidebarProvider, SidebarTrigger } from '../ui/sidebar';
 
 const Layout = () => {
   return (
     <SidebarProvider>
-    
-            <AppSidebar/>
-            <div className="h-screen overflow-y-scroll w-screen bg-[hsl(0,0%,98%)] dark:bg-[hsl(0,0%,0%)]">
-              <main className=" overflow-y-scroll rounded-lg md:h-[97vh] my-2  border mx-2 shadow-sm bg-[hsl(0,0%,100%)] dark:bg-[hsl(240,6%,7%)]">
-                <SidebarTrigger className="mx-2 my-2 absolute"/>
-                <Outlet/>
-             </main>
-            </div>
-       
-    
+      <AppSidebar />
+      <div className="h-screen overflow-y-scroll w-screen bg-[hsl(0,0%,98%)] dark:bg-[hsl(0,0%,0%)]">
+        <main className=" overflow-y-scroll overflow-x-hidden rounded-lg md:h-[97vh] my-2  border mx-2 shadow-sm bg-[hsl(0,0%,100%)] dark:bg-[hsl(240,6%,7%)]">
+          <SidebarTrigger className="mx-2 my-2 absolute  backdrop-blur-sm hover:bg-transparent" />
+          <Outlet />
+        </main>
+      </div>
     </SidebarProvider>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
