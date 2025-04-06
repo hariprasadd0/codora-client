59c57ce326c94d04cf3177414de14d5ae8b28be3
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-04-06 20:55:26 +0530

    refactor: reorganize imports and enhance component structure in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import appRoutes from './routes/appRoutes.tsx'
-import { ThemeProvider } from './components/theme-provider'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import appRoutes from './routes/appRoutes.tsx';
+import { ThemeProvider } from './components/theme-provider';
 
-const router = createBrowserRouter(appRoutes)
-
+const router = createBrowserRouter(appRoutes);
+const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <RouterProvider router={router} />
+    <ThemeProvider
+      defaultTheme="light"
+      storageKey="vite-ui-theme"
+    >
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ThemeProvider>
   </StrictMode>,
-)
+);
