4bfa984e543d20634345825b16c5fb389b65cbdb
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-04-06 20:56:07 +0530

    refactor: enhance layout component structure and improve sidebar handling

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,17 +1,33 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { AppSidebar } from '../sidebar/app-sidebar';
-import { SidebarProvider, SidebarTrigger } from '../ui/sidebar';
+import { SidebarProvider } from '../ui/sidebar';
+import Navbar from '../navbar/app-navbar';
+import { useEffect, useRef } from 'react';
+import { useSettingsStore } from '@/modules/users/store/user.store';
 
 const Layout = () => {
+  const location = useLocation();
+  const mainRef = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    mainRef.current?.scrollTo(0, 0);
+  }, [location.pathname]);
+
+  const side = useSettingsStore((state) => state.position);
   return (
     <SidebarProvider>
-      <AppSidebar />
-      <div className="h-screen overflow-y-scroll w-screen bg-[hsl(0,0%,98%)] dark:bg-[hsl(0,0%,0%)]">
-        <main className=" overflow-y-scroll overflow-x-hidden rounded-lg md:h-[97vh] my-2  border mx-2 shadow-sm bg-[hsl(0,0%,100%)] dark:bg-[hsl(240,6%,7%)]">
-          <SidebarTrigger className="mx-2 my-2 absolute  backdrop-blur-sm hover:bg-transparent" />
+      {side == 'left' && <AppSidebar side={side} />}
+
+      <div className="h-screen  w-screen bg-[hsl(0,0%,98%)] dark:bg-[hsl(0,0%,0%)]">
+        <main
+          ref={mainRef}
+          className=" overflow-x-hidden rounded-lg md:h-[97vh] my-2  border mx-2 shadow-sm bg-[hsl(0,0%,100%)] dark:bg-[hsl(240,6%,7%)]"
+        >
+          <Navbar location={location} />
           <Outlet />
         </main>
       </div>
+      {side == 'right' && <AppSidebar side={side} />}
     </SidebarProvider>
   );
 };
