e2c2a732ebe0c5bb8f506bb7ed6ed87e36061130
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-04-06 20:55:54 +0530

    refactor: standardize import formatting and improve route structure in appRoutes.tsx

--- src/routes/appRoutes.tsx
+++ src/routes/appRoutes.tsx
@@ -1,19 +1,22 @@
-import publicRoutes from "./publicRoutes"
-import protectedRoutes from "./protectedRoutes" 
-import AuthLayout from "@/components/layout/AuthLayout";
-import Layout from "@/components/layout/Layout";
-
+import publicRoutes from './publicRoutes';
+import protectedRoutes from './protectedRoutes';
+import AuthLayout from '@/components/layout/AuthLayout';
+import Layout from '@/components/layout/Layout';
+import { ProtectedRouteWrapper } from '@/components/ProtectedRouteWrapper';
 
 const appRoutes = [
-    {
-        element: <AuthLayout />,
-        children: publicRoutes
-    },
-    {
-        element: <Layout />,
-        children: protectedRoutes
-    },
-    
-]
+  {
+    element: <AuthLayout />,
+    children: publicRoutes,
+  },
+  {
+    element: (
+      <ProtectedRouteWrapper>
+        <Layout />
+      </ProtectedRouteWrapper>
+    ),
+    children: protectedRoutes,
+  },
+];
 
-export default appRoutes
+export default appRoutes;
