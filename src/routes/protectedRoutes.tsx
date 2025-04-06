1d468464c743ad0db7f75f559e8a08aa3ca960e1
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-04-06 20:54:18 +0530

    feat: update protected routes to include calendar and test page

--- src/routes/protectedRoutes.tsx
+++ src/routes/protectedRoutes.tsx
@@ -1,14 +1,26 @@
 import NotFound from '@/components/NotFound';
+import { Calendar } from '@/modules/calendar/Calendar.tsx';
+import Page from '@/modules/projects/components/table/page.tsx';
 import Task from '@/modules/tasks/components/Task.tsx';
 import Tasks from '@/modules/tasks/Tasks.tsx';
 import Settings from '@/modules/users/Settings';
 import { lazy } from 'react';
+import { Navigate } from 'react-router-dom';
 
 const Dashboard = lazy(() => import('../modules/users/Dashboard'));
 const Projects = lazy(() => import('../modules/projects/AllProjects.tsx'));
 const Project = lazy(() => import('../modules/projects/Project'));
 
 const protectedRoutes = [
+  {
+    path: '/',
+    element: (
+      <Navigate
+        to="/dashboard"
+        replace
+      />
+    ),
+  },
   {
     path: '/dashboard',
     element: <Dashboard />,
@@ -31,8 +43,16 @@ const protectedRoutes = [
     children: [{ path: '/tasks/:taskId', element: <Task /> }],
   },
   {
+    path: '/calendar',
+    element: <Calendar />,
+  },
+  {
     path: '*',
     element: <NotFound />,
+  },
+  {
+    path: '/test',
+    element: <Page />,
   },
 ];
 
