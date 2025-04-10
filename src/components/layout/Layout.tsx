101f918eb51b7a38d54126dba10d7ae1c417af6a
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-04-10 23:15:14 +0530

    feat: wrap AppSidebar in ErrorBoundary for improved error handling

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,6 +4,7 @@ import { SidebarProvider } from '../ui/sidebar';
 import Navbar from '../navbar/app-navbar';
 import { useEffect, useRef } from 'react';
 import { useSettingsStore } from '@/modules/users/store/user.store';
+import ErrorBoundary from '../ErrorBoundary';
 
 const Layout = () => {
   const location = useLocation();
@@ -16,18 +17,20 @@ const Layout = () => {
   const side = useSettingsStore((state) => state.position);
   return (
     <SidebarProvider>
-      {side == 'left' && <AppSidebar side={side} />}
+      <ErrorBoundary>
+        {side == 'left' && <AppSidebar side={side} />}
 
-      <div className="h-screen  w-screen bg-[hsl(0,0%,98%)] dark:bg-[hsl(0,0%,0%)]">
-        <main
-          ref={mainRef}
-          className=" overflow-x-hidden rounded-lg md:h-[97vh] my-2  border mx-2 shadow-sm bg-[hsl(0,0%,100%)] dark:bg-[hsl(240,6%,7%)]"
-        >
-          <Navbar location={location} />
-          <Outlet />
-        </main>
-      </div>
-      {side == 'right' && <AppSidebar side={side} />}
+        <div className="h-screen  w-screen bg-[hsl(0,0%,98%)] dark:bg-[hsl(0,0%,0%)]">
+          <main
+            ref={mainRef}
+            className=" overflow-x-hidden rounded-lg md:h-[97vh] my-2  border mx-2 shadow-sm bg-[hsl(0,0%,100%)] dark:bg-[hsl(240,6%,7%)]"
+          >
+            <Navbar location={location} />
+            <Outlet />
+          </main>
+        </div>
+        {side == 'right' && <AppSidebar side={side} />}
+      </ErrorBoundary>
     </SidebarProvider>
   );
 };
