9c2f07f6862d2cd30b3cbb3e48f6961e2e135455
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-04-13 14:05:28 +0530

    feat: add Toaster component for notifications in AuthLayout

--- src/components/layout/AuthLayout.tsx
+++ src/components/layout/AuthLayout.tsx
@@ -1,9 +1,14 @@
-import {Outlet} from 'react-router-dom'
+import { Outlet } from 'react-router-dom';
+import { Toaster } from '@/components/ui/sonner';
 
 export default function AuthLayout() {
-    return (
-        <section className="flex flex-col items-center justify-center w-full h-screen">
-            <Outlet/>
-        </section>
-    )
+  return (
+    <section className="flex flex-col items-center justify-center w-full h-screen">
+      <Outlet />
+      <Toaster
+        richColors
+        position="top-center"
+      />
+    </section>
+  );
 }
