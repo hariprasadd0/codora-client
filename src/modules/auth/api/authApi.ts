c8c974256795876397b307b4b71a9c55e1027b89
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-04-13 16:21:33 +0530

    feat: add password reset and creation mutations to auth API

--- src/modules/auth/api/authApi.ts
+++ src/modules/auth/api/authApi.ts
@@ -1,10 +1,15 @@
 import { apiClient } from '@/lib/api';
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { PasswordReset } from '../validation/schema';
 
 // add api to lib add interceptor for attaching the jwt token
 type LoginCredentials = {
   email: string;
   password: string;
+};
+type NewPassword = {
+  token: string;
+  newPassword: string;
 };
 
 export const useLogin = () => {
@@ -31,6 +36,26 @@ export const useSignUp = () => {
     },
     onSuccess: (data) => {
       queryClient.setQueryData(['user'], data.user);
+    },
+  });
+};
+
+export const usePasswordReset = () => {
+  return useMutation({
+    mutationFn: async (email: PasswordReset) => {
+      const response = await apiClient.post('users/request-reset', email);
+
+      return response.data;
+    },
+  });
+};
+
+export const usePasswordCreation = () => {
+  return useMutation({
+    mutationFn: async (data: NewPassword) => {
+      const response = await apiClient.post('users/reset-password', data);
+
+      return response.data;
     },
   });
 };
