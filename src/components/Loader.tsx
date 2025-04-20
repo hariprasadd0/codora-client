019c1726bf4dd3915641edf27e6e8a7c858abf17
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-04-20 23:47:39 +0530

    feat: add Loader

---
+++ src/components/Loader.tsx
@@ -0,0 +1,8 @@
+
+export const Loader = () => {
+    return (
+        <div className="w-full h-screen flex justify-center items-center font-semibold text-muted-foreground text-2xl">
+            C
+        </div>
+    )
+}
