5fb2eb462a98d066d1ffcbdace4e5105eb5a5807
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-05-12 23:35:35 +0530

    .

--- src/modules/auth/login.tsx
+++ src/modules/auth/login.tsx
@@ -136,7 +136,13 @@ const Login = () => {
           </div>
           <Button
             type="submit"
-            className="w-full h-10 my-4"
+            className="w-full h-10 my-4   relative inline-flex items-center justify-center gap-x-1.5 overflow-hidden
+    rounded-md px-3 py-1.5
+    shadow-[inset_0_0.75px_0_rgba(255,255,255,0.2),0_1px_2px_rgba(0,0,0,0.4),0_0_0_1px_#18181b]
+    transition-colors duration-150 ease-in-out
+    outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-[#3b82f6]
+    hover:bg-[#3f3f46]
+    active:bg-[#52525b]"
             disabled={isPending}
           >
             {isPending ? 'Logging in...' : 'Login'}
